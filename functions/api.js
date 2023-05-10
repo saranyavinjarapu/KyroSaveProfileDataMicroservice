884c88fd165e4fe895463b962ef0aa09faf30fa3
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2023-05-10 10:02:08 +0530

    added allow cors feature to enable backend calling from frontend

--- functions/api.js
+++ functions/api.js
@@ -13,6 +13,15 @@ app.use(
   })
 );
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 router.get("/", (request, response) => {
   response.json({
     message: "User Profile Details Microservice using NodeJS and Express ",
